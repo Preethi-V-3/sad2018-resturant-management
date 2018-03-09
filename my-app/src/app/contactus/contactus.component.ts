4cf7d9a5eec63043d1abf9d5775cb956bee6c726
Ashwini Patil <ashwini.patil228@gmail.com>
2018-03-09 22:33:21 +0100

    about component change

--- my-app/src/app/contactus/contactus.component.ts
+++ my-app/src/app/contactus/contactus.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'contactus.component.html',
-  styleUrls: ['contactus.component.css'],
+  selector: 'app-contactus',
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.css'],
 })
 export class ContactusComponent {
-  title: 'app';
   lat: number = 50.1109;
   lng: number = 8.6821;
 }
